c8ed1893fba39def0994458cd40697e751dc80b4
Aleksandr Andreev <alivemenone@gmail.com>
2021-01-12 13:18:00 +0300

    Add project files.

---
+++ OneLitre/OneLitre/CalculateViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using OneLitre.Annotations;
+
+namespace OneLitre
+{
+    public class CalculateViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private decimal _price;
+        private decimal _weight;
+        public decimal Cost { get; set; }
+
+
+
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+
+                Cost = CalculateTotal();
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Cost"));
+
+            }
+        }
+
+        public decimal Weight
+        {
+            get => _weight;
+            set
+            {
+                _weight = value;
+
+                Cost = CalculateTotal();
+                
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Cost"));
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private decimal CalculateTotal()
+        {
+            return (Price > 0 ? (1000 / Price) * Weight : 0) / 100;
+        }
+    }
+}
---
+++ OneLitre/OneLitre/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace OneLitre
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+            
+        }
+
+    }
+}
